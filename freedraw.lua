bcc83c304ad2101d4afd5e5e102d261073a5f1bd
BloxVault | Official <hd.ahles07@outlook.com>
2025-08-13 14:06:38 +0200

    Update freedraw.lua

--- freedraw.lua
+++ freedraw.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NaikoScript/Simple-Good-Scripts/main/FreeDraw"))(Settings)
+return function(Settings)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NaikoScript/Simple-Good-Scripts/main/FreeDraw"))(Settings)
+end
